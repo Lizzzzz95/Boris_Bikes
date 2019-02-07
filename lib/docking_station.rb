e1efa4ef16e6667acaf149a76f38099ea2c8a1ec
Lizzie Coady <lizziecoady@hotmail.co.uk>
2019-02-07 13:44:05 +0000

    Complete step 12, added error test and fail

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -5,9 +5,8 @@ class DockingStation
   attr_reader :bike
 
   def release_bike
-    # fail 'No bikes available' unless @bike
+    fail 'No bikes available' unless @bike
     @bike
-
   end
 
   def dock(bike)
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -9,10 +9,10 @@ describe DockingStation do
       subject.dock(bike)
       expect(subject.release_bike).to eq bike
     end
-    
-    # it 'raises an error when there are no bikes in the docking station' do
-    #   expect { subject.release_bike }.to raise_error 'No bikes available'
-    # end
+
+    it 'raises an error when there are no bikes in the docking station' do
+      expect {subject.release_bike}.to raise_error 'No bikes available'
+    end
 
   end
 
